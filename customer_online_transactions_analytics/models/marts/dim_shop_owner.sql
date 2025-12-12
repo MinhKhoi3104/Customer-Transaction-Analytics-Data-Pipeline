ff2cc9b5437c9591d05a4b5b48f8d2d02518b1cc
MinhKhoi3104 <khoinguyendata.work@gmail.com>
2025-12-12 12:11:44 +0700

    branch:main remove unnecessary step

--- customer_online_transactions_analytics/models/marts/dim_shop_owner.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-
--- dim table about shop and shop_owner
-select
-    shop_id,
-    shop_owner_uid
-from {{ ref('stg_transaction_data')}}
-group by shop_id, shop_owner_uid
